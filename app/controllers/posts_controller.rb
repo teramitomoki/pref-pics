5246731281017f754575d0e13f6a9c9be2a29b1e
teramitomoki <teramitomoki@gmail.com>
2021-10-22 04:57:32 +0900

    posts関係

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
+
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
