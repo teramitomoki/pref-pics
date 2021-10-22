bc01ac4e8dd09e1cecc3f4a9bd343d593756c2a3
teramitomoki <teramitomoki@gmail.com>
2021-10-22 22:56:20 +0900

    titleカラム追加

--- db/migrate/20211021174019_create_posts.rb
+++ db/migrate/20211021174019_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer	   :pref_id,  null: false
+      t.integer	   :prefecture_id,  null: false
       t.integer    :genre_id, null: false
+      t.string     :title
       t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
