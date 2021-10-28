018b4f0afae86f8c0ac2c7967cac566566c3ff60
teramitomoki <teramitomoki@gmail.com>
2021-10-28 15:53:21 +0900

    投稿削除機能、編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,25 @@ class PostsController < ApplicationController
     @user = @post.user_id
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to prefectures_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to prefectures_path
+  end
+
 
 
   private
